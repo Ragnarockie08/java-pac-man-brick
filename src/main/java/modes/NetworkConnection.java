b6da03bdc13308fa79dcfe9063573b4e5906c576
ragnarockie08 <ragnarockie08@gmail.com>
2018-03-06 17:46:58 +0100

    player movement added

--- src/main/java/modes/NetworkConnection.java
+++ src/main/java/modes/NetworkConnection.java
@@ -1,6 +1,7 @@
 package modes;
 
 
+import demo.Game;
 import javafx.scene.Node;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.Pane;
@@ -13,26 +14,31 @@ import java.io.Serializable;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public abstract class NetworkConnection {
+public abstract class NetworkConnection extends Thread {
 
     private Player player;
 
-    private ConnectionThread connectionThread = new ConnectionThread();
+    private Socket socket;
+    private ObjectOutputStream outputStream;
 
     public NetworkConnection(Player player) {
         this.player = player;
     }
 
     public void startConnection() throws Exception {
-        connectionThread.start();
+        start();
     }
 
     public void send(Serializable data) throws Exception {
-        connectionThread.outputStream.writeObject(data);
+        outputStream.writeObject(data);
     }
 
     public void closeConnection() throws Exception {
-        connectionThread.socket.close();
+        socket.close();
+    }
+
+    public Player getPlayer() {
+        return player;
     }
 
     protected abstract boolean isServer();
@@ -41,46 +47,41 @@ public abstract class NetworkConnection {
 
     protected abstract int getPort();
 
-    private class ConnectionThread extends Thread {
 
-        private Socket socket;
-        private ObjectOutputStream outputStream;
+    @Override
+    public void run() {
+        try {
 
-
-
-        @Override
-        public void run() {
-            try {
-
-                ServerSocket server = null;
-                Socket socket;
-                if (isServer()) {
-                    server = new ServerSocket(getPort());
-                    socket = server.accept();
-                } else {
-                    socket = new Socket(getIP(), getPort());
-                }
-
-                ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-                ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-
-                this.socket = socket;
-                this.outputStream = out;
-
-                while (true) {
-                    player = (Player) in.readObject();
-                    System.out.println("X :" + player.getxCoordinate());
-                    System.out.println("Y :" + player.getyCoordinate());
-
-                }
-
-            } catch (IOException e){
-
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
+            ServerSocket server = null;
+            Socket socket;
+            if (isServer()) {
+                server = new ServerSocket(getPort());
+                socket = server.accept();
+            } else {
+                socket = new Socket(getIP(), getPort());
             }
+
+            ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+            ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+
+            this.socket = socket;
+            this.outputStream = out;
+
+            while (true) {
+                Player player = (Player) in.readObject();
+                System.out.println(player.getxCoordinate());
+                System.out.println(player.getyCoordinate());
+                Game.getSquare().setTranslateY(player.getyCoordinate());
+                Game.getSquare().setTranslateX(player.getxCoordinate());
+            }
+
+        } catch (IOException e){
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
     }
+
 }
 
 
