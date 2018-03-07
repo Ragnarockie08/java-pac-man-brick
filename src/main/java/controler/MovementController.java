565c19928c030f7aef8de4cc2949dc37c1a2f523
smoczis <romanowski.mateusz93@gmail.com>
2018-03-07 18:22:46 +0100

    Create and fill table based on shapes on pane

--- src/main/java/controler/MovementController.java
+++ src/main/java/controler/MovementController.java
@@ -2,6 +2,7 @@ package controler;
 
 import demo.Game;
 import javafx.scene.Scene;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 import model.Player;
 import modes.NetworkConnection;
@@ -9,9 +10,15 @@ import modes.NetworkConnection;
 public class MovementController {
 
     private static final int STEP = 10;
+    private static char[][] walkableBoard = new char[680][680];
 
     public static void movement(Scene scene, Shape hostSquare, Shape clientSquare, NetworkConnection networkConnection) {
 
+        fillTable();
+        showWalkableBoard();
+        fillWithWalkableFields();
+        System.out.println();
+        showWalkableBoard();
         scene.setOnKeyPressed(event -> {
 
             try {
@@ -31,6 +38,8 @@ public class MovementController {
                         break;
                 }
 
+                System.out.println( (int) hostSquare.getTranslateX() + " : " + (int) hostSquare.getTranslateY() + "\n");
+
                 networkConnection.send(new Player(hostSquare.getTranslateX(), hostSquare.getTranslateY()));
 
             } catch (Exception e) {
@@ -40,7 +49,7 @@ public class MovementController {
     }
 
     private static void checkMoveUp(Shape player, Shape wall) {
-        if (player.getTranslateY() > 0
+        if (player.getTranslateY() > STEP
                 && !(player.getBoundsInParent().intersects(wall.getBoundsInParent())
                      && wall.getTranslateX() == player.getTranslateX()
                      && wall.getTranslateY() < player.getTranslateY())) {
@@ -49,7 +58,7 @@ public class MovementController {
     }
 
     private static void checkMoveDown(Shape player, Shape wall) {
-        if (player.getTranslateY() < Game.HEIGHT - 20
+        if (player.getTranslateY() < Game.HEIGHT - 40
                 && !(player.getBoundsInParent().intersects(wall.getBoundsInParent())
                      && wall.getTranslateX() == player.getTranslateX()
                      && wall.getTranslateY() > player.getTranslateY())) {
@@ -58,7 +67,7 @@ public class MovementController {
     }
 
     private static void checkMoveLeft(Shape player, Shape wall) {
-        if (player.getTranslateX() > 0
+        if (player.getTranslateX() > STEP
                 && !(player.getBoundsInParent().intersects(wall.getBoundsInParent())
                      && wall.getTranslateY() == player.getTranslateY()
                      && wall.getTranslateX() < player.getTranslateX())) {
@@ -67,11 +76,41 @@ public class MovementController {
     }
 
     private static void checkMoveRight(Shape player, Shape wall) {
-        if (player.getTranslateX() < Game.WIDTH - 20
+        if (player.getTranslateX() < Game.WIDTH - 40
                 && !(player.getBoundsInParent().intersects(wall.getBoundsInParent())
                      && wall.getTranslateY() == player.getTranslateY()
                      && wall.getTranslateX() > player.getTranslateX())) {
             player.setTranslateX(player.getTranslateX() + STEP);
         }
     }
+
+    private static void fillTable() {
+        for (int i=0; i<680; i++) {
+            for (int j=0; j<680; j++) {
+                walkableBoard[i][j] = 'O';
+            }
+        }
+    }
+
+    private static void showWalkableBoard() {
+        for (int i=0; i<680; i+=10) {
+            for (int j=0; j<680; j+=10) {
+                System.out.print(walkableBoard[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void fillWithWalkableFields() {
+
+
+        for (Rectangle shape : Game.getWalls()) {
+            for (int i=(int)shape.getLayoutX(); i<shape.getLayoutX() + shape.getWidth(); i++) {
+                for (int j=(int)shape.getLayoutY(); j<shape.getLayoutY() + shape.getHeight(); j++) {
+                    walkableBoard[i][j] = ' ';
+                }
+            }
+        }
+    }
+
 }
