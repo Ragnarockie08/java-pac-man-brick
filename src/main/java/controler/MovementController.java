21dd2bbef4dc206409e07b1b013be9e3cef283d7
ragnarockie08 <ragnarockie08@gmail.com>
2018-03-08 22:37:40 +0100

    add handleEnd method

--- src/main/java/controler/MovementController.java
+++ src/main/java/controler/MovementController.java
@@ -99,7 +99,7 @@ public class MovementController {
                             break;
                     }
                     handleCoins(pane);
-                    handleWin(networkConnection);
+                    handleEnd(networkConnection, pane);
                     roundDirection();
 
                     moved = true;
@@ -264,11 +264,34 @@ public class MovementController {
         }
     }
 
-    private void handleWin(NetworkConnection networkConnection){
+    private void handleEnd(NetworkConnection networkConnection, Pane pane){
 
         if (game.getCoins().isEmpty()){
             networkConnection.setConnected(false);
+            if (game.getMode() == Mode.SERVER){
+                handleWin(pane);
+            } else {
+                handleLose(pane);
+            }
+        } else if (game.getHostPlayer().getTranslateX() == game.getClientPlayer().getTranslateX()
+                && game.getClientPlayer().getTranslateX() == game.getClientPlayer().getTranslateY()){
+            if (game.getMode() == Mode.CLIENT){
+                handleWin(pane);
+            } else {
+                handleLose(pane);
+            }
         }
     }
+
+    private void handleWin(Pane pane){
+        Pane victoryPane = (Pane) pane.lookup("#victory");
+        victoryPane.setOpacity(1);
+    }
+
+    private void handleLose(Pane pane){
+
+    }
+
+
 
 }
